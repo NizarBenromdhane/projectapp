3d3fb5f0973071192661e1e53f3453ea38829acb
AnasMaiza01 <142988958+AnasMaiza01@users.noreply.github.com>
2024-12-19 21:17:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import participantRoutes from './routes/participantRoutes.js';
-import trainingRoutes from './routes/trainingRoutes.js';
+import participantRoutes from './participantRoutes.js';
+import instructorRoutes from './instructorRoutes.js";
+import trainingRoutes from './trainingRoutes.js';
 
 const app = express();
 
@@ -10,8 +11,9 @@ app.use(express.json());
 app.use(express.static('public'));  // Serve the frontend static files from 'public'
 
 // Routes
-app.use(participantRoutes);
-app.use(trainingRoutes);
+app.use("/participants", participantRoutes);
+app.use("/instructors", instructorRoutes);
+app.use("/trainings",trainingRoutes);
 
 // MongoDB connection
 mongoose.connect('mongodb://127.0.0.1:27017/trainingManagement', {
